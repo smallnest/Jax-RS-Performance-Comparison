57a9aada0648aeb639cb588bad93fbfbf18e2531
Gelin Luo <greenlaw110@gmail.com>
2018-06-01 19:53:03 +1000

    add actframework

---
+++ act/src/main/java/benchmark/act/AppEntry.java
@@ -0,0 +1,19 @@
+package benchmark.act;
+
+import act.Act;
+
+/**
+ * A simple hello world service app entry
+ *
+ * Run this app, try to update some of the code, then
+ * press F5 in the browser to watch the immediate change
+ * in the browser!
+ */
+@SuppressWarnings("unused")
+public class AppEntry {
+
+    public static void main(String[] args) throws Exception {
+        Act.start();
+    }
+
+}
